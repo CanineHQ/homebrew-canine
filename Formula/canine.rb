5833cbd7da048c71f86c85f91631fc18af2840fa
Chris <chris@example.com>
2025-12-31 10:22:24 -0800

    update sha

--- Formula/canine.rb
+++ Formula/canine.rb
@@ -1,8 +1,8 @@
 class Canine < Formula
   desc "Kubernetes CLI for Canine"
-  homepage "https://github.com/CanineHQ/canine-cli"
-  url "https://github.com/CanineHQ/canine-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  homepage "https://github.com/CanineHQ/cli"
+  url "https://github.com/CanineHQ/cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "503ea8bcba013c7c48237665d3541e048028cffd6e957ad1146725bf4e8a416a"
   license "MIT"
 
   depends_on "rust" => :build
