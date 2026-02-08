66755c17a8bda88f57fa34d7d467af46b1218366
Chris <chris@example.com>
2026-02-07 18:47:29 -0800

    Update canine to v0.1.30

--- Formula/canine.rb
+++ Formula/canine.rb
@@ -1,8 +1,8 @@
 class Canine < Formula
   desc "Kubernetes CLI for Canine"
   homepage "https://github.com/CanineHQ/cli"
-  url "https://github.com/CanineHQ/cli/archive/refs/tags/v0.1.29.tar.gz"
-  sha256 "633dbb0626ef9a21b4d077ad19b3ecdd51324fac9fc82eb403d59269ebdb9018"
+  url "https://github.com/CanineHQ/cli/archive/refs/tags/v0.1.30.tar.gz"
+  sha256 "4d3e6e60acc6d86901c08b7f8876e34ef4ad8d5d35742db8d4846c04e81141f2"
   license "MIT"
 
   depends_on "rust" => :build
